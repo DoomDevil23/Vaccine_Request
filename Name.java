790fc2a0dedfbaf9bd129838cd2c84791f234684
DoonDevil23 <orlandos723@gmail.com>
2021-08-11 13:05:12 -0500

    Getters and setters added.Adding comments

--- Name.java
+++ Name.java
@@ -3,10 +3,13 @@ package Vaccine_Request;
 public class Name {
     private String firstName, lastName;
 
+    /*****************CLASS CONSTRUCTOR*****************/
     public Name(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    /***************** GETTERS *****************/
 
     public String get_firstName(){
         return firstName;
@@ -16,6 +19,17 @@ public class Name {
         return lastName;
     }
 
+    /***************** SETTERS *****************/
+
+    public void set_firstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public void set_lastName(String lastName){
+        this.lastName=lastName;
+    }
+
+    /***************** TOSTRING METHOD. RETURNS CLASS ATTRIBUTES. LASTNAME AND FIRSTNAME WITH A COMMA SEPARATOR *****************/
     public String toString(){
         return lastName+","+firstName;
     }
