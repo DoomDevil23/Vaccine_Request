db724a786765239e2bc04cd621e3a4b654f1a126
DoonDevil23 <orlandos723@gmail.com>
2021-08-16 02:09:19 -0500

    code readability improved

--- Vaccines_Request.java
+++ Vaccines_Request.java
@@ -60,166 +60,57 @@ public class Vaccines_Request{
             switch(choice){
                 //CASE 1 FOR ADDING CLIENTS TO VACCINE CENTRE
                 case 1:
-                    ClientV objClientV;
-
                     clearScreen();
-                    
-                    System.out.println("Enter Clients First Name:");
-                    String firstName = System.console().readLine();
-
-                    System.out.println("Enter Clients Last Name:");
-                    String lastName = System.console().readLine();
-
-                    System.out.println("Enter Clients ID:");
-                    String idClient = System.console().readLine();
-
-                    System.out.println("Enter Clients Phone:");
-                    String phoneClient = System.console().readLine();
-
-                    objClientV = new ClientV(idClient, phoneClient, firstName, lastName);
-                    objvaccinecentre.add_client(objClientV);
-                    
-                    System.out.println("Client has been added. Press Enter to continue.");
-                    System.console().readLine();
-                    
+                    option_one(objvaccinecentre);
                     clearScreen();
                     break;
 
                 //CASE 2 FOR REMOVING CLIENTS FROM VACCINE CENTRE CLIENTS LIST
                 case 2: 
                     clearScreen();
-                    System.out.println("Enter client id code to remove:");
-                    String id_client = System.console().readLine();
-                    objvaccinecentre.remove_client(id_client);
-                    System.out.println("Press Enter to continue.");
-                    System.console().readLine();
+                    option_two(objvaccinecentre);
                     clearScreen();
                     break;
 
                 //CASE 3 FOR RECORDING A SPECIFIC CLIENTS FIRST VACCINATION
                 case 3:
-                    int company_id=0, batch_number=0;
-                    String company_name="";
-                    LocalDate vaccine_date = LocalDate.now();
-                    boolean complation_status = false;
-
                     clearScreen();
-
-                    System.out.println("Enter client code to find:");
-                    String idclient = System.console().readLine();
-                    
-                    int index = objvaccinecentre.find_clientIndex(idclient);
-                    if(index==-1){
-                        System.out.println("This client doesnt exist!");
-                    }
-                    else{
-                        //READING THE CODE FOR SELECTED VACCINE
-                        //LOOP UNTIL SELECT A VALID VALUE
-                        while(!(company_id<=4) || !(company_id>=1)){
-                            System.out.println("1 - Pfizer");
-                            System.out.println("2 - Astra-Zenica");
-                            System.out.println("3 - Moderna");
-                            System.out.println("4 - Jhonson&Johnson");
-                            System.out.println("Enter vaccine code number:");
-                            
-                            try{
-                                company_id = Integer.parseInt(System.console().readLine());
-                                
-                                //VALIDATING COMPANY_ID IN RIGHT RANGE
-                                if(company_id>4 || company_id<1){
-                                    System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-                                    System.out.println("Invalid value of:"+company_id+". Enter a number between 1-4");
-                                }
-                            }
-                            //ERROR THROWS WHEN INPUT IS NOT INTEGER VALUE
-                            catch(Exception e){
-                                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-                                System.out.println("Enter a number between 1-4");
-                            }
-                        }
-
-                        //SELECTING COMPANY NAME ACCORDING TO COMPANY CODE
-                        switch (company_id) {
-                            case 1:
-                                company_name="Pfizer";
-                                break;
-
-                            case 2:
-                                company_name="Astra-Zenica";
-                                break;
-
-                            case 3:
-                                company_name="Moderna";
-                                break;
-
-                            case 4:
-                                company_name="Johnson&Johnson";
-                                complation_status=true;
-                                break;
-                        }
-
-                        //LOOP FOR GETTING RIGHT BATCH NUMBER
-                        while(batch_number==0){
-                            System.out.println("Enter batch number for this vaccine:");
-                            try{
-                                batch_number = Integer.parseInt(System.console().readLine());
-                            }
-                            catch(Exception e){
-                                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-                                System.out.println("Batch number is incorrect. Please try again with a valid batch number.");
-                            }
-                        }
-                        ClientV objclientactual = objvaccinecentre.find_client(index);
-                        ClientV objclientupdate = new ClientV(objclientactual.get_idClient(), objclientactual.get_phoneClient(), objclientactual.get_firstName(), objclientactual.get_lastName(), company_name, company_id, batch_number, vaccine_date, complation_status);
-                        objvaccinecentre.addvaccineclient(index, objclientupdate);
-                    }
-
+                    option_three(objvaccinecentre);
+                    clearScreen();
                     break;
 
                 //CASE 4 FOR SHOWING ALL THE CLIENTS FOR THIS VACCINE CENTRE
                 case 4:
                     clearScreen();
-                    objvaccinecentre.show_all_clients();
-                    System.out.println("This are the registered clients for this vaccine centre. Press Enter to continue.");
-                    System.console().readLine();
+                    option_four(objvaccinecentre);
                     clearScreen();
                     break;
                
                 //CASE 5 FOR SHOWING STATISTICS OF CLIENTS GIVEN EACH VACCINE TYPE
                 case 5:
                     clearScreen();
-                    objvaccinecentre.show_stats();
-                    System.out.println("Press Enter to continue");
-                    System.console().readLine();
+                    option_five(objvaccinecentre);
                     clearScreen();
                     break;
 
                 //CASE 6 FOR SHOWING CLIENTS DUE A SECOND VACCINE AND THE DATE DUE
                 case 6:
                     clearScreen();
-                    objvaccinecentre.show_clients_due();
-                    System.out.println("Press Enter to continue");
-                    System.console().readLine();
+                    option_six(objvaccinecentre);
                     clearScreen();
                     break;
 
                 //CASE FOR LOADING CLIENTS DATA SAVED IN A TEXT FILE
                 case 7:
                     clearScreen();
-                    objvaccinecentre.load_client_data(file, objvaccinecentre);
-                    System.out.println("Clients have been uploaded from txt file!");
-                    System.out.println("Press Enter to continue");
-                    System.console().readLine();
+                    option_seven(file, objvaccinecentre);
                     clearScreen();
                     break;
 
                 //CASE FOR SAVING CLIENTS DATA IN A TXT FILE
                 case 8:
                     clearScreen();
-                    objvaccinecentre.save_client_data(file);
-                    System.out.println("The clients have been saved!");
-                    System.out.println("Press Enter to continue.");
-                    System.console().readLine();
+                    option_eight(file, objvaccinecentre);
                     clearScreen();
                     break;
             }
@@ -231,5 +122,159 @@ public class Vaccines_Request{
     public static void clearScreen(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 1 IN MENU. THIS FUNCTION READS CLIENT DATA AND SAVE IT IN THEIR CLASS OBJECTS **********/
+    public static void option_one(VaccineCentre objvaccinecentre){
+        System.out.println("Enter Clients First Name:");
+        String firstName = System.console().readLine();
+
+        System.out.println("Enter Clients Last Name:");
+        String lastName = System.console().readLine();
+
+        System.out.println("Enter Clients ID:");
+        String idClient = System.console().readLine();
+
+        System.out.println("Enter Clients Phone:");
+        String phoneClient = System.console().readLine();
+
+        ClientV objClientV = new ClientV(idClient, phoneClient, firstName, lastName);
+        objvaccinecentre.add_client(objClientV);
+        
+        System.out.println("Client has been added. Press Enter to continue.");
+        System.console().readLine();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 2 IN MENU. THIS FUNCTION READS CLIENT ID AND SENDS IT TO VACCINE_CENTRE CLASS TO DELETE IT IF EXIST **********/
+    public static void option_two(VaccineCentre objvaccinecentre){
+        System.out.println("Enter client id code to remove:");
+        String id_client = System.console().readLine();
+        objvaccinecentre.remove_client(id_client);
+        System.out.println("Press Enter to continue.");
+        System.console().readLine();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 3 IN MENU. THIS FUNCTION READS CLIENT ID AND ADDS DATA FOR A NEW VACCINATION COMPLETED **********/
+    public static void option_three(VaccineCentre objvaccinecentre){
+        //NEEDED ATTRIBUTES FOR REGISTERING A VACCINATION
+        int company_id=0, batch_number=0, index;
+        String company_name="";
+        LocalDate vaccine_date = LocalDate.now();
+        boolean complation_status = false;
+
+        //READING CLIENT ID
+        System.out.println("Enter client code to find:");
+        String idclient = System.console().readLine();
+        
+        //LOOKING AND GETTING CLIENT INDEX IN LIST IF CLIENT ID EXISTS IN CLIENTS LIST
+        index = objvaccinecentre.find_clientIndex(idclient);
+        if(index==-1){
+            System.out.println("This client doesnt exist!");
+        }
+        else{
+            //READING THE CODE FOR SELECTED VACCINE
+            //LOOP UNTIL SELECT A VALID VALUE
+            while(!(company_id<=4) || !(company_id>=1)){
+                System.out.println("1 - Pfizer");
+                System.out.println("2 - Astra-Zenica");
+                System.out.println("3 - Moderna");
+                System.out.println("4 - Jhonson&Johnson");
+                System.out.println("Enter vaccine code number:");
+                
+                try{
+                    company_id = Integer.parseInt(System.console().readLine());
+                    
+                    //VALIDATING COMPANY_ID IN RIGHT RANGE
+                    if(company_id>4 || company_id<1){
+                        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                        System.out.println("Invalid value of:"+company_id+". Enter a number between 1-4");
+                    }
+                }
+                //ERROR THROWS WHEN INPUT IS NOT INTEGER VALUE
+                catch(Exception e){
+                    System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                    System.out.println("Enter a number between 1-4");
+                }
+            }
+
+            //SELECTING COMPANY NAME ACCORDING TO COMPANY CODE
+            switch (company_id) {
+                case 1:
+                    company_name="Pfizer";
+                    break;
+
+                case 2:
+                    company_name="Astra-Zenica";
+                    break;
+
+                case 3:
+                    company_name="Moderna";
+                    break;
+
+                case 4:
+                    company_name="Johnson&Johnson";
+                    complation_status=true;
+                    break;
+            }
+
+            //LOOP FOR GETTING RIGHT BATCH NUMBER
+            while(batch_number==0){
+                System.out.println("Enter batch number for this vaccine:");
+                try{
+                    batch_number = Integer.parseInt(System.console().readLine());
+                }
+                catch(Exception e){
+                    System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                    System.out.println("Batch number is incorrect. Please try again with a valid batch number.");
+                }
+            }
+
+            //CLIENT VACCINE INFORMATION UPDATE
+            ClientV objclientactual = objvaccinecentre.find_client(index);
+            ClientV objclientupdate = new ClientV(objclientactual.get_idClient(), objclientactual.get_phoneClient(), objclientactual.get_firstName(), objclientactual.get_lastName(), company_name, company_id, batch_number, vaccine_date, complation_status);
+            objvaccinecentre.addvaccineclient(index, objclientupdate);
+
+            System.out.println("Successful vaccination!");
+            System.out.println("Press Enter to continue");
+            System.console().readLine();
+        }
+
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 4 IN MENU. THIS FUNCTION USES VACCINE_CENTRE CLASS TO SHOW ALL REGISTERED CLIENTS FOR THE CURRENT VACCINE CENTRE **********/
+    public static void option_four(VaccineCentre objvaccinecentre){
+        objvaccinecentre.show_all_clients();
+        System.out.println("This are the registered clients for this vaccine centre. Press Enter to continue.");
+        System.console().readLine();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 5 IN MENU. THIS FUNCTION USES VACCINE_CENTRE CLASS TO SHOW VACCINATED CLIENTS STATS **********/
+    public static void option_five(VaccineCentre objvaccinecentre){
+        objvaccinecentre.show_stats();
+        System.out.println("Press Enter to continue");
+        System.console().readLine();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 6 IN MENU. THIS FUNCTION USES VACCINE_CENTRE CLASS TO SHOW CLIENTS NEEDING A SECOND DOSE **********/
+    public static void option_six(VaccineCentre objvaccinecentre){
+        objvaccinecentre.show_clients_due();
+        System.out.println("Press Enter to continue");
+        System.console().readLine();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 7 IN MENU. THIS FUNCTION USES VACCINE_CENTRE CLASS TO LOAD CLIENTS FROM A TXT FILE **********/
+    public static void option_seven(File file, VaccineCentre objvaccinecentre){
+        objvaccinecentre.load_client_data(file, objvaccinecentre);
+        System.out.println("Clients have been uploaded from txt file!");
+        System.out.println("Press Enter to continue");
+        System.console().readLine();
+    }
+
+    /********** FUNCTION FOR MANAGING OPTION 8 IN MENU. THIS FUNCTION USES VACCINE_CENTRE CLASS TO SAVE CLIENTS INTO A TXT FILE **********/
+    public static void option_eight(File file, VaccineCentre objvaccinecentre){
+        objvaccinecentre.save_client_data(file);
+        System.out.println("The clients have been saved!");
+        System.out.println("Press Enter to continue.");
+        System.console().readLine();
     }
 }
